87546e92c028d184125f0cbc28df87e827851953
Jeong-Wonju <wj.jeong@mozzet.com>
2022-02-14 10:30:58 +0900

    초기화 위치 수정

--- Assets/Joystick Pack/Examples/JoystickSetterExample.cs
+++ Assets/Joystick Pack/Examples/JoystickSetterExample.cs
@@ -12,6 +12,11 @@ public class JoystickSetterExample : MonoBehaviour
 
     private void Awake()
     {
+        
+    }
+
+    private void Start()
+    {
         GameObjectManager.instance.my_joystick = this;
     }
 
