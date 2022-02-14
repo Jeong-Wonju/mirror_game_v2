c6e6a6e4e58d02dda99798ddc8616fcbac65276e
Jeong-Wonju <wj.jeong@mozzet.com>
2022-02-14 10:25:25 +0900

    초기화 위치 수정

--- Assets/Dissonance/Integrations/MirrorIgnorance/Demo/MirrorIgnorancePlayerController.cs
+++ Assets/Dissonance/Integrations/MirrorIgnorance/Demo/MirrorIgnorancePlayerController.cs
@@ -5,6 +5,13 @@ namespace Dissonance.Integrations.MirrorIgnorance.Demo
 {
     public class MirrorIgnorancePlayerController : NetworkBehaviour
     {
+        private JoystickSetterExample joystic;
+
+        private void Start()
+        {
+            joystic = GameObjectManager.instance.my_joystick;
+        }
+
         private void Update()
         {
             if (!isLocalPlayer)
@@ -12,20 +19,39 @@ namespace Dissonance.Integrations.MirrorIgnorance.Demo
                 return;
             }
 
-            var controller = GetComponent<CharacterController>();
+            //var controller = GetComponent<CharacterController>();
             
             var rotation = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var speed = Input.GetAxis("Vertical") * 3.0f;
+            var speed = 5;
             
             transform.Rotate(0, rotation, 0);
-            var forward = transform.TransformDirection(Vector3.forward);
-            controller.SimpleMove(forward * speed);
+            
+            if(joystic.variableJoystick.Direction.x != 0 || joystic.variableJoystick.Direction.y != 0)
+            {
+                Debug.Log("joystick pos : " + joystic.variableJoystick.Direction);
+            }
 
-            if (transform.position.y < -3)
+            Vector3 forward = new Vector3(joystic.variableJoystick.Direction.x, 0, joystic.variableJoystick.Direction.y);
+
+            if (joystic.variableJoystick.Direction.x != 0 || joystic.variableJoystick.Direction.y != 0)
+            {
+                Debug.Log("forward  : " + forward);
+                Debug.Log("speed  : " + speed);
+            }
+
+            Vector3 pos = transform.position;
+
+            pos = pos + forward * speed;
+
+            transform.position = pos;
+
+            //controller.SimpleMove(forward * speed);
+
+            /*if (transform.position.y < -3)
             {
                 transform.position = Vector3.zero;
                 transform.rotation = Quaternion.identity;
-            }
+            }*/
         }
     }
 }
